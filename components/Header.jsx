34164e677a15fc5a9fee1d18cdf42cb36e2f2df4
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-08-26 22:42:00 +0300

    Add search box in the middle of header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function Header() {
   return (
-    <div>
-          <div className="flex items-center justify-between max-w-6xl">
+      <div className="flex items-center justify-between max-w-6xl">
+          {/* left section */}
               <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
                   <Image
                       src='/instagramLogosm.png'
@@ -20,9 +21,16 @@ export default function Header() {
                       className='object-contain'
                       alt='instagram Logo'
                   />
-        </div>
+          </div>
+          
+          {/* middle section */}
+          <div className='relative mt-1'>
+              <div className='absolute top-2 left-2'> 
+                  <MagnifyingGlassIcon className= 'h-5 text-gray'/>
+              </div>
+              <input type="text" placeholder='search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' />
+          </div>
         <div>Right Section</div>
       </div>
-    </div>
   )
 }
