1dbd2634772545991306b6b721bc2b56f7eb8816
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-09-04 16:46:15 +0300

    Create post comment, post input box and post button

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { EllipsisHorizontalIcon, HeartIcon, ChatBubbleOvalLeftIcon, BookmarkIcon } from "@heroicons/react/24/outline"
+import { EllipsisHorizontalIcon, HeartIcon, ChatBubbleLeftEllipsisIcon , BookmarkIcon, FaceSmileIcon  } from "@heroicons/react/24/outline"
 
 export default function Post({img, userImg, username, caption, id}) {
   return (
@@ -20,10 +20,21 @@ export default function Post({img, userImg, username, caption, id}) {
           <div className='flex justify-between px-4 pt-4'>
               <div className='flex items-center space-x-4'>
                   <HeartIcon className='btn'/>
-                  <ChatBubbleOvalLeftIcon className='btn'/>
+                  <ChatBubbleLeftEllipsisIcon  className='btn'/>
               </div>
               <BookmarkIcon className='btn' />
           </div>
+
+          {/* Post comment */}
+          <p className='p-5 truncate'><span className='font-bold mr-2'>{username}</span>{caption}</p>
+
+          {/* Post Input box */}
+          
+          <form className='flex items-center p-4 FaceSmileIcon space-x-4'>
+              <FaceSmileIcon className='h-7' />
+              <input className='border-none flex-1 focus:ring-0 rounded-md bg-gray-100' type="text" placeholder='Enter your comment...' />
+              <button className='text-blue-400 font-bold'>Post</button>
+          </form>
     </div>
   )
 }
